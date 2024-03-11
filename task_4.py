9e5dccb35ce743d26c139c1509729fcd2d4f2999
mdoktor7 <you@example.com>
2024-03-11 23:01:23 +0200

    2nd try

--- task_4.py
+++ task_4.py
@@ -1,70 +1,46 @@
 from datetime import datetime, timedelta
 
-# Список користувачів з їхніми датами народження
+
+def get_upcoming_birthdays(users):   
+    # Поточна дата
+    today = datetime.today().date()
+    # Список майбутніх днів народження
+    upcoming_birthdays = []
+    # Ітерація по підготовленим користувачам
+    for user in users:
+        # Парсимо дату народження
+        birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()
+        # Заміна року на поточний для дня народження цього року
+        birthday_this_year = birthday.replace(year=today.year)
+
+        # Якщо дата народження вже пройшла цього року
+        if birthday_this_year < today:
+        # Переносимо наступний рік
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+        # Визначаємо кі-сть днів до дня народження
+        days_before_birthday = (birthday_this_year - today).days
+        # Якщо день народження випадає на суботу або неділю
+        if days_before_birthday <=7:
+            if birthday_this_year.weekday() >= 5:
+            # Знаходимо наступний понеділок
+                birthday_this_year += timedelta(days=(7 - birthday_this_year.weekday()))
+            
+        # Додаємо дані про майбутній день народження
+            upcoming_birthdays.append({
+                "name": user["name"],
+                "congratulation_date": birthday_this_year.strftime("%Y.%m.%d")
+            })
+
+    return upcoming_birthdays
+
 users = [
     {"name": "John Doe", "birthday": "1985.01.23"},
     {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Jane Smith1", "birthday": "1990.03.05"},
-    {"name": "Jane Smith1", "birthday": "1990.03.07"},
-    {"name": "Jane Smith1", "birthday": "1990.03.10"},
+    {"name": "Jane Smak", "birthday": "1990.03.16"},
+    {"name": "June Smit", "birthday": "1990.03.27"},
+    {"name": "Jane Wither", "birthday": "1990.03.17"},
 ]
 
-def prepared_users(users):
-    # Список підготовлених користувачів
-    formated_users = []
-    # Ітерація по кожному користувачеві зі списку
-    for user in users:
-        try:
-            # Парсимо дату народження
-            birthday = datetime.strptime(user['birthday'], '%Y.%m.%d').date()
-            # Додаємо користувача з підготовленою датою народження
-            formated_users.append({"name": user['name'], 'birthday': birthday})
-        except ValueError:
-            # Виводимо повідомлення про помилку
-           print(f'Некоректна дата народження для користувача {user["name"]}')
-    return formated_users
-
-
-def find_next_weekday(d, weekday: int):
-    days_ahead = weekday - d.weekday()
-    if days_ahead <= 0:
-        days_ahead += 7
-    return d + timedelta(days=days_ahead)
-
-
-def get_upcoming_birthdays(prepared_users):
-    # Список майбутніх днів народження
-    upcoming_birthdays = []
-    # Кількість днів для перевірки на наближені дні народження
-    days = 7
-    # Поточна дата
-    today = datetime.today().date()
-    # Ітерація по підготовленим користувачам
-    for user in prepared_users:
-        # Заміна року на поточний для дня народження цього року
-        birthday_this_year = user["birthday"].replace(year=today.year)
-
-        # Якщо дата народження вже пройшла цього року
-    if birthday_this_year < today:
-        # Переносимо наступний рік
-        birthday_this_year = birthday_this_year.replace(year=today.year + 1)
-
-        # Якщо день народження в межах вказаного періоду
-    if 0 <= (birthday_this_year - today).days <= days:
-        # Якщо день народження випадає на суботу або неділю
-        if birthday_this_year.weekday() >= 5:
-            # Знаходимо наступний понеділок
-            birthday_this_year = find_next_weekday(birthday_this_year, 0)
-
-            # Форматуємо дату у рядок
-        congratulation_date_str = birthday_this_year.strftime('%Y.%m.%d')
-        # Додаємо дані про майбутній день народження
-        upcoming_birthdays.append({
-            "name": user["name"],
-            "congratulation_date": congratulation_date_str
-        })
-    return upcoming_birthdays
-
-formatted_users = prepared_users(users)
-upcoming_birthdays = get_upcoming_birthdays(formatted_users)
+upcoming_birthdays = get_upcoming_birthdays(users)
 print(upcoming_birthdays)
